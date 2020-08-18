92d0e3492504d6d91d34f1ea41ae82f2e87c2433
Declan Ordish <declan.ordish@hotmail.co.uk>
2020-08-18 15:58:56 +0100

    tweaked schema, + seeded bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2020_08_18_094308) do
-
-
+ActiveRecord::Schema.define(version: 2020_08_18_114757) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,7 +19,7 @@ ActiveRecord::Schema.define(version: 2020_08_18_094308) do
     t.datetime "start_date"
     t.datetime "end_date"
     t.integer "price"
-    t.boolean "confirmed"
+    t.boolean "confirmed", default: false
     t.bigint "vehicle_id", null: false
     t.bigint "user_id", null: false
     t.datetime "created_at", precision: 6, null: false
