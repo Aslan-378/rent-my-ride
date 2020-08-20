7382c5ab6ba61b88f5d0ca96f8eee99317f7f0ed
Martin Darras <darras.martin@gmail.com>
2020-08-20 13:43:55 +0200

    Added map on vehcile index

--- app/controllers/vehicles_controller.rb
+++ app/controllers/vehicles_controller.rb
@@ -1,14 +1,39 @@
 class VehiclesController < ApplicationController
   def index
     @vehicles = Vehicle.all
+
+    @geo_vehicles = Vehicle.geocoded
+
+    @markers = @geo_vehicles.map do |vehicle|
+      {
+        lat: vehicle.latitude,
+        lng: vehicle.longitude
+      }
+    end
   end
 
   def new
     @vehicle = Vehicle.new
   end
 
+  def create
+    @vehicle = Vehicle.new(vehicle_params)
+    @vehicle.user = current_user
+    if @vehicle.save
+      redirect_to vehicle_path(@vehicle)
+    else
+      render :new
+    end
+  end
+
   def show
     @vehicle = Vehicle.find(params[:id])
     @booking = Booking.new
+  end
+
+  private
+
+  def vehicle_params
+    params.require(:vehicle).permit(:category, :model, :price, :description, :address)
   end
 end
